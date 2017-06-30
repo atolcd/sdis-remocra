4a160017b4706563d253f1e2702e61bb94a207c7
Charles-Henry Vagner <cva@atolcd.com>
2017-06-30 16:02:09 +0200

    Script de mise à jour des serveurs
    * Choix du fichier
    
    Change-Id: Ie0adc463395caa15b718753cbedd7fcdc7ae8efa

--- packaging/update-servers/upd.sh
+++ packaging/update-servers/upd.sh
@@ -3,25 +3,32 @@
 
 cd $(dirname $0)
 
-if [ $# -lt 1 ] || [ ! -f remocra.war ]
+if [ $# -lt 1 ]
 then
-  echo 'usage: upd.sh servername [port]'
-  echo '       remocra.war must be in the current directory'
+  echo 'usage: upd.sh servername [port] [warfile]'
+  echo '       warfile : remocra.war par défaut'
   exit 1
 fi
 
 serveur=$1
-
-if [ $# -le 2 ]
-then
-    export port=$2
-fi
+port=$2
 port=${port:=22}
+warfile=$3
+warfile=${warfile:=remocra.war}
 
-echo "       host : $serveur, port : $port"
+if [ ! -f ${war} ]
+then
+  echo 'usage: upd.sh servername [port] [warfile]'
+  echo '       warfile : remocra.war par défaut'
+  echo ''
+  echo "${warfile} non trouvé"
+  exit 1
+fi
+
+echo "       host : $serveur, port : $port, file : ${warfile}"
 
 echo '       transfert du war'
-scp -P $port remocra.war root@$serveur:/home/postgres/
+scp -P $port ${warfile} root@$serveur:/home/postgres/remocra.war
 
 echo '       mise à jour du serveur'
 ssh $serveur -p $port -l root < upd.txt
