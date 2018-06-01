d0c98a321ac9eb0e21dd1e9a50f80739fbc8888e
rai <rai@atolcd.com>
2018-06-01 09:32:45 +0200

    Ajout dans l'identifiant carroyage dans table SGO traca
    Change-Id: If8ed3ff0e250103d64b45727647384173674fcaf

--- server/sdis-remocra/home/postgres/remocra_db/patches/sdis/77/085_003.sql
+++ server/sdis-remocra/home/postgres/remocra_db/patches/sdis/77/085_003.sql
@@ -6,5 +6,8 @@ INSERT INTO remocra_sgo.hydrant_nature (code_nature_sgo, code_nature_remocra) VA
 
 UPDATE remocra_sgo.hydrant_nature SET code_nature_remocra = 'PI_DIAM80' WHERE code_nature_remocra = 'PI_DIAM70';
 
+ALTER TABLE remocra_sgo.hydrant_tracabilite ADD COLUMN carroyage character varying;
+COMMENT ON COLUMN remocra_sgo.hydrant_tracabilite.carroyage IS 'Identifiant du carré de 200 de la table remocra_referentiel.carre_200';
+
 commit;
 
