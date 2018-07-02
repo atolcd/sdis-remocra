ae4ff01ab037eb18afc6ff9e39cc168fafd865d1
Charles-Henry Vagner <cva@atolcd.com>
2018-07-02 16:27:35 +0200

    Passage en 0.9.20
    Change-Id: I0a3def9cd7613eab25d7b15ee656b4cd129bb969

--- remocra/src/main/java/fr/sdis83/remocra/web/RemocraController.java
+++ remocra/src/main/java/fr/sdis83/remocra/web/RemocraController.java
@@ -47,7 +47,7 @@ public class RemocraController {
     // Automatiquement défini lors du packaging (ne pas toucher cette ligne)
     public static String REVISION_NUMBER = "";
 
-    public static String VERSION_NUMBER = "0.9.19";
+    public static String VERSION_NUMBER = "0.9.20";
 
     @Autowired
     private AuthoritiesUtil authUtils;
