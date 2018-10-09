d19e12f1def71d50cbb51f8d2ec7a6261167c944
Charles-Henry Vagner <cva@atolcd.com>
2018-10-09 09:30:46 +0200

    Launcher : valeur par défaut REMOCRA_PRESERVE_DOTKETTLE
    Change-Id: Id5fbdfd129223b99e44b6ae818da706d672d67aa

--- vagrant_ext/livraison/launcher.sh
+++ vagrant_ext/livraison/launcher.sh
@@ -58,6 +58,7 @@ export APP_IGNKEYS=${APP_IGNKEYS:-"fjwf53vbh2ikn9q009g6mi7f"}
 export GEOSERVER_URL_CIBLE=${GEOSERVER_URL_CIBLE:-localhost}
 
 export REMOCRA_PRESERVE_REMOCRACONF=${REMOCRA_PRESERVE_REMOCRACONF:-false}
+export REMOCRA_PRESERVE_DOTKETTLE=${REMOCRA_PRESERVE_DOTKETTLE:-false}
 
 ### Paramètres cachés réservés à un usage interne dans un contexte Box Vagrant par exemple
 # (REMOCRA_FORCE_CREATE_DB_IF_ABSENT : true/false)
