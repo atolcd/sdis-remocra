9dd2fb8905275cceb98df6e1ebe1f7daf7150067
Charles-Henry Vagner <cva@atolcd.com>
2018-12-19 16:56:20 +0100

    SDIS 42 : trg_calcul_debit_pression_42
    Change-Id: I6670564e0dba4b44ed8bf76f151735b98c1ad74d

---
+++ server/sdis-remocra/home/postgres/remocra_db/patches/sdis/42/097_002.sql
@@ -0,0 +1,55 @@
+begin;
+
+
+CREATE OR REPLACE FUNCTION remocra.calcul_debit_pression_42(id_hydrant bigint)
+  RETURNS void AS
+$BODY$
+DECLARE
+	p_anomalie_id integer;
+	p_rec remocra.hydrant_pibi%ROWTYPE;
+BEGIN
+	select * into p_rec from remocra.hydrant_pibi where id = id_hydrant;
+
+	-- Suppression des anomalies débit/pression des règles communes
+	delete from remocra.hydrant_anomalies where hydrant=id_hydrant and anomalies in (select id from remocra.type_hydrant_anomalie where code IN('PRESSION_INSUFF', 'PRESSION_TROP_ELEVEE', 'PRESSION_DYN_INSUFF', 'PRESSION_DYN_TROP_ELEVEE', 'DEBIT_INSUFF', 'DEBIT_TROP_ELEVE', 'DEBIT_INSUFF_NC'));
+
+	-- Suppression des anciennes anomalies
+	delete from remocra.hydrant_anomalies where hydrant = p_rec.id and anomalies in (select id from remocra.type_hydrant_anomalie where code IN('DEBIT_EGAL_0'));
+
+	-- Ajout des anomalies
+	if (p_rec.debit = 0) then
+		select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'DEBIT_EGAL_0';
+		insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
+	end if;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100;
+ALTER FUNCTION remocra.calcul_debit_pression_42(bigint) OWNER TO postgres;
+GRANT EXECUTE ON FUNCTION remocra.calcul_debit_pression_42(bigint) TO public;
+GRANT EXECUTE ON FUNCTION remocra.calcul_debit_pression_42(bigint) TO postgres;
+GRANT EXECUTE ON FUNCTION remocra.calcul_debit_pression_42(bigint) TO remocra;
+
+CREATE OR REPLACE FUNCTION remocra.trg_calcul_debit_pression_42() RETURNS trigger AS
+$BODY$
+DECLARE
+	p_rec record;
+BEGIN
+	if (TG_OP = 'DELETE') then
+		p_rec = OLD;
+	else
+		p_rec = NEW;
+	end if;
+	perform remocra.calcul_debit_pression_42(p_rec.id);
+
+    RETURN p_rec;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100;
+ALTER FUNCTION remocra.trg_calcul_debit_pression_42() OWNER TO postgres;
+CREATE TRIGGER trig_debit_pression_42 AFTER INSERT OR UPDATE ON remocra.hydrant_pibi FOR EACH ROW EXECUTE PROCEDURE remocra.trg_calcul_debit_pression_42();
+
+
+commit;
+
