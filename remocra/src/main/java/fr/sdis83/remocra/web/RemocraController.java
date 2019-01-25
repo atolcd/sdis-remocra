278190bea19f6341d410549c9bfe0932ae78344e
Charles-Henry Vagner <cva@atolcd.com>
2019-01-25 16:23:08 +0100

    Version 0.11.2
    Change-Id: I2db4ffdc953b23e097ac9955b9fe6ad615508615

--- remocra/src/main/java/fr/sdis83/remocra/web/RemocraController.java
+++ remocra/src/main/java/fr/sdis83/remocra/web/RemocraController.java
@@ -47,7 +47,7 @@ public class RemocraController {
     // Automatiquement défini lors du packaging (ne pas toucher cette ligne)
     public static String REVISION_NUMBER = "";
 
-    public static String VERSION_NUMBER = "0.11.1";
+    public static String VERSION_NUMBER = "0.11.2";
 
     @Autowired
     private AuthoritiesUtil authUtils;
