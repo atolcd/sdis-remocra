62f12f256c6fc0f707150d056d4bc9ba718d014b
rai <rai@atolcd.com>
2019-04-03 11:14:29 +0200

    78 : Traitement de synchronisation LDAP
    Change-Id: I663cd02a4f431e64927b95b1821c147b3ea108a1

---
+++ server/sdis-remocra/var/remocra/pdi/traitements_sdis/78/synchroniser_ldap.sh
@@ -0,0 +1,3 @@
+# Synchroniser les utilisateurs entre le LDAP et Remocra
+cd /home/postgres/pdi
+sh kitchen.sh -file:"/var/remocra/pdi/traitements_sdis/78/ldap/synchroniser_ldap_remocra.kjb" -level:Error -param:"PDI_FICHIER_PARAMETRE=/home/postgres/remocra_pdi/remocra.properties"
