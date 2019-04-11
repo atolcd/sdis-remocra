675f02bc92c95e373552e75e3760fb5b257567d0
Charles-Henry Vagner <cva@atolcd.com>
2019-04-11 09:50:35 +0200

    cts : gestion via repository maven local
    Change-Id: Ifa681410c56aa363793181c0aee2cec894a6a340

--- remocra/package.sh
+++ remocra/package.sh
@@ -11,8 +11,6 @@ cd $(dirname $0)
 
 export MAVEN_OPTS="-Xms512m -Xmx1536m -XX:PermSize=512m -XX:MaxPermSize=1024m"
 
-mvn install:install-file -Dfile=lib/irstv-cts.jar -DgroupId=org.cts -DartifactId=cts -Dversion=1.69 -Dpackaging=jar
-
 REVNUMBER=$(git log -1 --pretty=format:%h | awk '{print substr($1, 1, 5)}')
 # Java 7 : TLS 1.2 désactivé par défaut dans les versions avant 1.7.0_131-b31. Activation avec "-Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2"
 mvn -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 clean verify -Dmaven.test.failure.ignore=false -P modeinfo-able
