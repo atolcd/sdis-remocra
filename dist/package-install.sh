4728e36dec7212114cecb9f1c5e409a4b0adb426
Yasser AZAIEZ <yaz@atolcd.com>
2019-05-14 14:25:17 +0200

    Build avec client-ng
    Change-Id: Ib9792b2ab1af4c4b97bfdc9ce5a8170c8988d2ce

--- dist/package-install.sh
+++ dist/package-install.sh
@@ -8,6 +8,6 @@ docker run --rm \
   -u $(id -u):$(id -g) \
   -v "$(pwd)":/app -w /app \
   -v ~/.m2:/var/maven/.m2 -e MAVEN_CONFIG=/var/maven/.m2 -e MAVEN_OPTS="-Duser.home=/var/maven" \
-  cvagner/docker-jdk-maven-sencha-cmd:7-3.6.0-3.0.2 \
+  cvagner/docker-jdk-maven-sencha-cmd:7-3.6.1-3.0.2 \
   \
   mvn clean verify -P modeinfo-able -P install
--- remocra/src/main/webapp/js/build/build-docker.sh
+++ remocra/src/main/webapp/js/build/build-docker.sh
@@ -21,7 +21,7 @@ docker run --rm \
   -u $(id -u):$(id -g) \
   -v `pwd`:/app -w /app \
   -v ~/.m2:/var/maven/.m2 -e MAVEN_CONFIG=/var/maven/.m2 -e MAVEN_OPTS="-Duser.home=/var/maven" \
-  cvagner/docker-jdk-maven-sencha-cmd:7-3.6.0-3.0.2 \
+  cvagner/docker-jdk-maven-sencha-cmd:7-3.6.1-3.0.2 \
   \
   sencha compile -classpath=openlayers-2.12/OpenLayers.js,openlayers-2.12/lib/OpenLayers/Lang/fr.js,proj4js-1.1.0/lib/proj4js-compressed.js,proj4js-1.1.0/lib/defs/EPSG3857.js,ext-4.1.1a/src,ext-4.1.1a/locale/ext-lang-fr.js,app/remocra \
     -debug=false \
