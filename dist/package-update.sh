1ed4567c82c16ae209f6116043402f8d5c0961a9
Charles-Henry Vagner <cva@atolcd.com>
2019-05-14 23:30:37 +0200

    Fix docker package client-ng (EACCESS)
    Change-Id: If842b0fa51d61a36819f2b0ab951d60b10d9f05d

--- dist/package-update.sh
+++ dist/package-update.sh
@@ -6,8 +6,12 @@ cd $(dirname $0)/..
 docker run --rm \
   --name remocra \
   -u $(id -u):$(id -g) \
-  -v "$(pwd)":/app -w /app \
-  -v ~/.m2:/var/maven/.m2 -e MAVEN_CONFIG=/var/maven/.m2 -e MAVEN_OPTS="-Duser.home=/var/maven" \
+  -w /app \
+  -v "$(pwd)":/app \
+  -v "${HOME}":/var/maven \
+  -e HOME=/var/maven \
+  -e MAVEN_CONFIG=/var/maven/.m2 \
+  -e MAVEN_OPTS="-Duser.home=/var/maven" \
   cvagner/docker-jdk-maven-sencha-cmd:7-3.6.1-3.0.2 \
   \
   mvn clean verify -P modeinfo-able -P update
