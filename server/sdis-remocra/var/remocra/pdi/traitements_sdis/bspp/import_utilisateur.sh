445dcb6bf954b11614a7b3d836268e03e28d4a89
Roger AIRES <rai@atolcd.com>
2019-07-29 16:20:26 +0200

    BSPP: Synchronisation des utilisateurs avec le LDAP
    Change-Id: Ic4c4b8b783d8dec3bfa6cb0a86699e7009cae683

--- server/sdis-remocra/var/remocra/pdi/traitements_sdis/bspp/import_utilisateur.sh
+++ server/sdis-remocra/var/remocra/pdi/traitements_sdis/bspp/import_utilisateur.sh
@@ -1,3 +1,3 @@
 # Intégration des utilisateurs via un export csv du LDAP
-cd /home/postgres/pdi
+cd /home/postgres/pdi7.1
 sh kitchen.sh -file:"/var/remocra/pdi/traitements_sdis/bspp/utilisateur/synchroniser_ldap_remocra.kjb" -level:Error -param:"PDI_FICHIER_PARAMETRE=/home/postgres/remocra_pdi/remocra.properties"
