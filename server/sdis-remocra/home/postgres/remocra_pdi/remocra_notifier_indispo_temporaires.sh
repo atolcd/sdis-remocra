5f00266bf811dfdfb5cab3294e174506dc3f5f1a
Roger AIRES <rai@atolcd.com>
2019-08-19 10:31:11 +0200

    Script lancement indispo temporaire
    Change-Id: I803e3ccf38cf12e914fb354ba447ea6893be5625

--- server/sdis-remocra/home/postgres/remocra_pdi/remocra_notifier_indispo_temporaires.sh
+++ server/sdis-remocra/home/postgres/remocra_pdi/remocra_notifier_indispo_temporaires.sh
@@ -1,6 +1,6 @@
 # Tâche principale de création des notifications de début et de fin d'indisponibilité temporaire d'hydrant
 # Périodicité : A DETERMINER
 
-cd /home/postgres/pdi
-sh kitchen.sh -rep:"ref_pdi_remocra" -dir:"maintenance/indisponibilites_temporaires" -job:"creer_notifications_indisponibilites" -user:admin -pass:admin -level:Error -param:"PDI_FICHIER_PARAMETRE=/home/postgres/remocra_pdi/remocra.properties"
+cd /home/postgres/pdi7.1
+sh kitchen.sh -file:"/var/remocra/modeles/processus_etl/commun/maintenance/indisponibilite_temporaire/creer_notifications_indisponibilites.kjb" -level:Error -param:"PDI_FICHIER_PARAMETRE=/home/postgres/remocra_pdi/remocra.properties"
 
