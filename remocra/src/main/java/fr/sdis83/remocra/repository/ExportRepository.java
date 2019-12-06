9e590515486c4aea3901543b15a81621d0ed50d9
Yasser AZAIEZ <yaz@atolcd.com>
2019-12-06 15:58:37 +0100

    Export des documents des process ETL :zap:
    Change-Id: I6627ad653744e13bd6418503b7aabe3ef72e8d3e

---
+++ remocra/src/main/java/fr/sdis83/remocra/repository/ExportRepository.java
@@ -0,0 +1,86 @@
+package fr.sdis83.remocra.repository;
+
+import static fr.sdis83.remocra.db.model.remocra.Tables.DOCUMENT;
+import static fr.sdis83.remocra.db.model.remocra.Tables.PROCESSUS_ETL_DOCUMENT;
+import static org.jooq.impl.DSL.row;
+
+import java.io.File;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import fr.sdis83.remocra.db.model.remocra.tables.pojos.Document;
+import fr.sdis83.remocra.db.model.remocra.tables.pojos.ProcessusEtlDocument;
+import org.apache.log4j.Logger;
+import org.joda.time.Instant;
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+@Configuration
+public class ExportRepository {
+  private final Logger logger = Logger.getLogger(getClass());
+
+
+  @Autowired
+  DSLContext context;
+
+
+  public ExportRepository() {
+
+  }
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public ExportRepository exportRepository(DSLContext context) {
+    return new ExportRepository(context);
+  }
+
+  ExportRepository(DSLContext context) {
+    this.context = context;
+  }
+
+  public String getExportFilePathFromCode(String code) {
+    try {
+      ProcessusEtlDocument ped = context.select().from(PROCESSUS_ETL_DOCUMENT).where(PROCESSUS_ETL_DOCUMENT.CODE.eq(code)).fetchInto(ProcessusEtlDocument.class).get(0);
+      if (ped == null) {
+        return null;
+      }
+      Document d = context.select().from(DOCUMENT).join(PROCESSUS_ETL_DOCUMENT).on(DOCUMENT.ID.eq(PROCESSUS_ETL_DOCUMENT.DOCUMENT)).where(PROCESSUS_ETL_DOCUMENT.CODE.eq(code)).fetchInto(Document.class).get(0);
+      if (d == null) {
+        return null;
+      }
+      return d.getRepertoire() + File.separator + d.getFichier();
+    } catch (EmptyResultDataAccessException e) {
+      return null;
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+
+  public boolean setExportAccuseFromCode(String code) {
+    try {
+      ProcessusEtlDocument ped = context.select().from(PROCESSUS_ETL_DOCUMENT).where(PROCESSUS_ETL_DOCUMENT.CODE.eq(code)).fetchInto(ProcessusEtlDocument.class).get(0);
+
+      if (ped == null) {
+        return false;
+      } else if (ped.getAccuse() == null) {
+        ped.setAccuse(new Instant());
+        context.update(PROCESSUS_ETL_DOCUMENT)
+            .set(row(PROCESSUS_ETL_DOCUMENT.ACCUSE)
+                ,row(new Instant()))
+            .where(PROCESSUS_ETL_DOCUMENT.ID.eq(ped.getId())).execute();
+      }
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+
+}
---
+++ remocra/src/main/java/fr/sdis83/remocra/web/ExportController.java
@@ -0,0 +1,60 @@
+package fr.sdis83.remocra.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import flexjson.JSONSerializer;
+import fr.sdis83.remocra.db.model.remocra.tables.pojos.ProcessusEtl;
+import fr.sdis83.remocra.db.model.remocra.tables.pojos.ProcessusEtlModele;
+import fr.sdis83.remocra.db.model.remocra.tables.pojos.ProcessusEtlModeleParametre;
+import fr.sdis83.remocra.domain.remocra.RemocraVueCombo;
+import fr.sdis83.remocra.repository.ExportRepository;
+import fr.sdis83.remocra.repository.ProcessusEtlModeleParametereRepository;
+import fr.sdis83.remocra.repository.ProcessusEtlModeleRepository;
+import fr.sdis83.remocra.util.DocumentUtil;
+import fr.sdis83.remocra.web.message.ItemFilter;
+import fr.sdis83.remocra.web.serialize.ext.AbstractExtListSerializer;
+import fr.sdis83.remocra.web.serialize.ext.SuccessErrorExtSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+@RequestMapping("/exports")
+@Controller
+public class ExportController {
+
+
+    @Autowired
+    private ProcessusEtlModeleRepository processusEtlModeleRepository;
+
+  @Autowired
+  private ProcessusEtlModeleParametereRepository processusEtlModeleParametereRepository;
+
+
+@Autowired
+private ExportRepository exportRepository;
+
+
+  @RequestMapping(value = "/{code}")
+  @PreAuthorize("hasRight('TRAITEMENTS_C')")
+  public void downloadExportDocument(@PathVariable("code") String code, HttpServletResponse response)
+      throws IOException {
+    String path = exportRepository.getExportFilePathFromCode(code);
+    DocumentUtil.getInstance().downloadDocument(path, code, response);
+    // Accusé de réception
+    exportRepository.setExportAccuseFromCode(code);
+  }
+}
