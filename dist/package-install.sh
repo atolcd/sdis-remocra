152a17bda727ee90a2ac7c40bfe3c922416682f5
Charles-Henry Vagner <cva@atolcd.com>
2019-12-09 18:40:07 +0100

    Passage à Java 8 (compilation)
    Change-Id: If63cbb3600866095ffda8449cd23a7bee8ceb378

--- dist/package-install.sh
+++ dist/package-install.sh
@@ -12,6 +12,6 @@ docker run --rm \
   -e HOME=/var/maven \
   -e MAVEN_CONFIG=/var/maven/.m2 \
   -e MAVEN_OPTS="-Duser.home=/var/maven" \
-  cvagner/docker-jdk-maven-sencha-cmd:7-3.6.1-3.0.2 \
+  cvagner/docker-jdk-maven-sencha-cmd:8-3.6.3-3.0.2 \
   \
   mvn clean verify -P modeinfo-able -P install
