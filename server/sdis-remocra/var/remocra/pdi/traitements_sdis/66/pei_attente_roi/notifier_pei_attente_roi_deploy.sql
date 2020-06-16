d01115a6727acec685002bc9aa86c05c3b5b5cff
Roger AIRES <rai@atolcd.com>
2020-06-16 11:27:55 +0200

    SDIS66 : Notifier des ROI à réaliser
    Traitement PDI de notification des ROI à réaliser
    
    Change-Id: I66e13066cc78d5470a8e5c3aa7a03c13df281544

---
+++ server/sdis-remocra/var/remocra/pdi/traitements_sdis/66/pei_attente_roi/notifier_pei_attente_roi_deploy.sql
@@ -0,0 +1,17 @@
+INSERT INTO remocra.email_modele (code, corps, objet) VALUES ('MODELE_MAIL_ATTENTE_ROI', '<title>Remocra - Des PEI sont en attente de ROI</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<style type="text/css">
+    div{width:800px;text-align:justify;}
+    p{text-decoration:none;color:#000000;font-family: arial;font-size: 14px;}
+    p.footer{text-decoration:none;font-style: italic;color:#AAAAAA;font-family: arial;font-size: 12px;}
+    p.caution{text-decoration:none;font-style: italic;color:#000000;font-family: arial;font-size: 12px;}
+    table{border-collapse:collapse;}
+    td{text-decoration:none;border-width:1px;border-style:solid;color:#000000;font-family: arial;font-size: 14px;}
+</style>
+<div>
+    <p>Bonjour,<br/>
+    <br/>[LISTE_PEI]<br/>
+    <br/>Cordialement.</p>
+    <p class="caution">En cas d''incompréhension de ce message, merci de prendre contact avec le SDIS.</p>
+    <p class="footer">Ce message vous a été envoyé automatiquement, merci de ne pas répondre à l''expéditeur.</p>
+</div>', 'Remocra - Des PEI sont en attente de ROI');
