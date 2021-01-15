a48a242b4c838fa32277de7efd9f077a0998d3e3
Yasser AZAIEZ <yaz@atolcd.com>
2021-01-15 17:08:06 +0100

    Intégration du Open-api dans l'api-remocra [api]
    Change-Id: I8f4182ed5e98d9bfed13bc256ba72338fd07a56f

--- api-remocra/api/src/main/java/fr/sdis83/remocra/web/s/OpenApiEndpoint.java
+++ api-remocra/api/src/main/java/fr/sdis83/remocra/web/s/OpenApiEndpoint.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-@Path("/openapi.{type:json|yaml}")
+@Path("/openapi")
 public class OpenApiEndpoint extends BaseOpenApiResource {
 @Context
 ServletConfig config;
@@ -25,6 +25,7 @@ ServletConfig config;
 Application app;
 
 @GET
+@Path("openapi.{type:json|yaml}")
 @Produces({MediaType.APPLICATION_JSON, "application/yaml"})
 @Operation(hidden = true)
 @PermitAll
@@ -33,4 +34,44 @@ public Response getOpenApi(@Context HttpHeaders headers,
                            @PathParam("type") String type) throws Exception {
         return super.getOpenApi(headers, config, app, uriInfo, type);
 }
+
+@GET
+@Produces(MediaType.TEXT_HTML)
+@Operation(hidden = true)
+@PermitAll
+public Response showOpenApi() {
+        return Response.ok()
+                .entity(
+                        "\n"
+                                + "<!DOCTYPE html>\n"
+                                + "<html>\n"
+                                + "<head>\n"
+                                + "  <meta charset=\"UTF-8\">\n"
+                                + "  <title>Bee technologies - API</title>\n"
+                                + "  <link rel=\"stylesheet\" type=\"text/css\""
+                                + " href=\"//unpkg.com/swagger-ui-dist@3/swagger-ui.css\">\n"
+                                + "  <script"
+                                + " src=\"//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js\"></script>\n"
+                                + "  <script"
+                                + " src=\"//unpkg.com/swagger-ui-dist@3/swagger-ui-standalone-preset.js\"></script>\n"
+                                + "</head>\n"
+                                + "<body>\n"
+                                + "  <script>\n"
+                                + "window.onload = function() {\n"
+                                + "const ui = SwaggerUIBundle({\n"
+                                + "    url: \"openapi/openapi.yaml\",\n"
+                                + "    dom_id: '#swagger-ui',\n"
+                                + "    presets: [\n"
+                                + "      SwaggerUIBundle.presets.apis,\n"
+                                + "      SwaggerUIBundle.SwaggerUIStandalonePreset\n"
+                                + "    ],\n"
+                                + "  })\n"
+                                + "}\n"
+                                + "</script>\n"
+                                + "<div id=\"swagger-ui\">\n"
+                                + "</body>\n"
+                                + "</html>\n")
+                .build();
+}
+
 }
