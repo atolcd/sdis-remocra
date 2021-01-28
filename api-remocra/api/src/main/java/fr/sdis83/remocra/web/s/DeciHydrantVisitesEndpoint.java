e5a7c15ff670a7555a4aa0f993ba659605d29b08
Romain Bourg <rbo@atolcd.com>
2021-01-28 09:33:10 +0100

    API : Fix méthode PUT visites spécifiques
    Change-Id: Icd0b4bfeb70f90e91af3faec7b59dd36a309aa42

--- api-remocra/api/src/main/java/fr/sdis83/remocra/web/s/DeciHydrantVisitesEndpoint.java
+++ api-remocra/api/src/main/java/fr/sdis83/remocra/web/s/DeciHydrantVisitesEndpoint.java
@@ -16,6 +16,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -82,7 +83,7 @@ public class DeciHydrantVisitesEndpoint {
     return Response.ok(hydrantVisitesRepository.getHydrantVisiteSpecifique(numero, idVisite), MediaType.APPLICATION_JSON).build();
   }
 
-  @POST
+  @PUT
   @Path("/{idVisite}")
   @Operation(summary = "Modifie une visite spécifique", tags = {"DECI - Visites"})
   @ApiResponse(responseCode = "200", description = "Visite modifiée avec succès")
