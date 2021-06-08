7500774e292db6d8dfbbd654b8e8f925149b69a2
Roger AIRES <rai@atolcd.com>
2021-06-08 16:38:33 +0200

    SDIS66 : grant des droits pour template_sgo
    Change-Id: Iaf97966cd2ff3312f64dea919034101544eb75e1

---
+++ server/sdis-remocra/home/postgres/remocra_db/patches/sdis/66/149_003.sql
@@ -0,0 +1,8 @@
+begin
+
+GRANT USAGE ON SCHEMA remocra_sgo TO sgo_template;
+GRANT SELECT ON TABLE remocra_sgo.hydrant_disponibilite TO sgo_template;
+GRANT SELECT ON TABLE remocra_sgo.hydrant_nature TO sgo_template;
+GRANT SELECT ON TABLE remocra_sgo.hydrant TO sgo_template;
+
+commit;
