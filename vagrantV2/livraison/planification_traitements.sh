15ae62f105cc1e936330204abd03768bb16e53bf
Yasser AZAIEZ <yaz@atolcd.com>
2022-01-05 15:39:49 +0100

    Ajout de planification des traitements (courriers,zone_competence_commune)
    Change-Id: I87ceff8f7e8b07add4f80fa15d1c3a2ce816bbb4

--- vagrantV2/livraison/planification_traitements.sh
+++ vagrantV2/livraison/planification_traitements.sh
@@ -117,3 +117,43 @@ curl -v http://localhost:8070/v1/jobs -XPOST -d '{
     "command": "/home/pdi/kitchen.sh -file:'/var/remocra/modeles/processus_etl/commun/maintenance/indisponibilite_temporaire/creer_notifications_indisponibilites.kjb' -level:Error -param:'PDI_FICHIER_PARAMETRE=/home/pdi/remocra.properties' >> /var/remocra/pdi/log/remocra_notifier_indispo_temporaires.log 2>&1"
   }
 }'
+
+# Référencement des modèles de courrier
+curl -v http://localhost:8070/v1/jobs -XPOST -d '{
+  "name": "referencer-courriers",
+  "displayname": "Référencer les modèles de courrier",
+  "schedule": "0 0 0 * * *",
+  "owner": "",
+  "owner_email": "",
+  "disabled": false,
+  "tags": {
+    "pdi": "7.1:1"
+  },
+  "metadata": { },
+  "retries": 0,
+  "concurrency": "forbid",
+  "executor": "shell",
+  "executor_config": {
+    "command": "/home/pdi/kitchen.sh -file:/var/remocra/modeles/processus_etl/commun/maintenance/referencer_modele_courrier/referencer_les_modeles_de_courrier.kjb -level:Error -param:PDI_FICHIER_PARAMETRE=/home/pdi/remocra.properties >> /var/remocra/pdi/log/remocra_referencer_modele_courrier.log 2>&1"
+  }
+}'
+
+# Mise à jour la table zone_competence_commune
+curl -v http://localhost:8070/v1/jobs -XPOST -d '{
+  "name": "maj-zcc",
+  "displayname": "Mettre à jour la table zone_competence_commune",
+  "schedule": "0 0 1 * * *",
+  "owner": "",
+  "owner_email": "",
+  "disabled": false,
+  "tags": {
+    "pdi": "4.4:1"
+  },
+  "metadata": null,
+  "retries": 0,
+  "concurrency": "forbid",
+  "executor": "shell",
+  "executor_config": {
+    "command": "/home/pdi/kitchen.sh -rep:ref_pdi_remocra -dir:maintenance/zone_competence_commune -job:actualisation_zone_competence_commune -user:admin -pass:admin -level:Error -param:PDI_FICHIER_PARAMETRE=/home/pdi/remocra.properties >> /var/remocra/pdi/log/remocra_zone_competence_commune.log 2>&1"
+  }
+}'
