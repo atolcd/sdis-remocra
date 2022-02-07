489d0c2e1e71d3abd18a580e4dedfa5e0c2351f6
Romain BOURG <rbo@atolcd.com>
2022-02-07 09:13:56 +0100

    Renommage paramètre de configuration
    Change-Id: Iaab3179c71b86d4d1743af67a1ff2d300bb20ef4

---
+++ server/sdis-remocra/home/postgres/remocra_db/patches/160/160.sql
@@ -0,0 +1,42 @@
+begin;
+
+set statement_timeout = 0;
+set client_encoding = 'UTF8';
+set standard_conforming_strings = off;
+set check_function_bodies = false;
+set client_min_messages = warning;
+set escape_string_warning = off;
+
+set search_path = remocra, pdi, public, pg_catalog;
+
+--------------------------------------------------
+-- Versionnement du patch et vérification
+--
+create or replace function versionnement_dffd4df4df() returns void language plpgsql AS $body$
+declare
+    numero_patch int;
+    description_patch varchar;
+begin
+    -- Métadonnées du patch
+    numero_patch := 160;
+    description_patch := 'Couverture hydraulique : Fix réglage des isodistances non prises en compte';
+
+    -- Vérification
+    if (select numero_patch-1 != (select max(numero) from remocra.suivi_patches)) then
+        raise exception 'Le numéro de patch requis n''est pas le bon. Dernier appliqué : %, en cours : %', (select max(numero) from remocra.suivi_patches), numero_patch; end if;
+    -- Suivi
+    insert into remocra.suivi_patches(numero, description) values(numero_patch, description_patch);
+end $body$;
+select versionnement_dffd4df4df();
+drop function versionnement_dffd4df4df();
+
+--------------------------------------------------
+-- Contenu réel du patch début
+
+UPDATE remocra.param_conf SET cle = 'DECI_ISODISTANCES' CHERE cle = 'PLANIFICATION_DECI_ISODISTANCES';
+
+
+-- Contenu réel du patch fin
+--------------------------------------------------
+
+commit;
