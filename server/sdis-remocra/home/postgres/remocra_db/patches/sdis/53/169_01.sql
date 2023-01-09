e82ec39466b68210d4b85ac6919eed14a5da0d3f
Emilie Genton <emg@atolcd.com>
2023-01-09 11:18:51 +0100

    SDIS 53 : Met à jour la fonction calcul_debit_pression_53...
    ... Change les codes d'anomalies et ajoute un THEN oublié
    
    Issue: #163361
    Change-Id: I219fb79b8efad5f6573c90a300d346ba1bc60a6e

--- server/sdis-remocra/home/postgres/remocra_db/patches/sdis/53/169_01.sql
+++ server/sdis-remocra/home/postgres/remocra_db/patches/sdis/53/169_01.sql
@@ -4,47 +4,44 @@ CREATE OR REPLACE FUNCTION remocra.calcul_debit_pression_53(id_hydrant bigint)
  LANGUAGE plpgsql
 AS $function$
 DECLARE
-	p_anomalie_id integer;
-	p_rec remocra.hydrant_pibi%ROWTYPE;
+    p_anomalie_id integer;
+    p_rec remocra.hydrant_pibi%ROWTYPE;
 BEGIN
-	select * into p_rec from remocra.hydrant_pibi where id = id_hydrant;
-	-- Suppression des anomalies débit/pression des règles communes
-	delete from remocra.hydrant_anomalies where hydrant=id_hydrant and anomalies in (select id from remocra.type_hydrant_anomalie where code IN('PRESSION_INSUFF_NC', 'PRESSION_TROP_ELEVEE', 'PRESSION_DYN_INSUFF', 'PRESSION_DYN_TROP_ELEVEE', 'DEBIT_INSUFF_NC', 'DEBIT_TROP_ELEVE', 'DEBIT_INSUFF_NORME_NC'));
+    select * into p_rec from remocra.hydrant_pibi where id = id_hydrant;
+    -- Suppression des anomalies débit/pression des règles communes
+    delete from remocra.hydrant_anomalies where hydrant=id_hydrant and anomalies in (select id from remocra.type_hydrant_anomalie where code IN('PRESSION_INSUFF', 'PRESSION_TROP_ELEVEE', 'PRESSION_DYN_INSUFF', 'PRESSION_DYN_TROP_ELEVEE', 'DEBIT_INSUFF_NC', 'DEBIT_TROP_ELEVE', 'DEBIT_INSUFF'));
 
-  
-	-- Ajout des anomalies DEBIT
+    -- Ajout des anomalies DEBIT
 
    -- NULL OU Q <= 15 ===> INDISPO
-	if (p_rec.debit is null
-		OR p_rec.debit <= 15)
-	then 
-		select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'DEBIT_INSUFF';
-		insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
+    if (p_rec.debit is null
+        OR p_rec.debit <= 15)
+    then
+        select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'DEBIT_INSUFF';
+        insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
 
-	-- 15 m3 / h   Q  ≤ 59 m3 / h ===> NON_CONFORME
-	elsif (p_rec.debit > 15 AND p_rec.debit < 60)
-	then
-		select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'DEBIT_INSUFF_NC';
-		insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
-	--  Q > 1000 m3 / h ===> Trop elevée
-	elsif (p_rec.debit > 1000 )
-	then
-		select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'DEBIT_TROP_ELEVE';
-		insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
-	end if;
-	
-
+    -- 15 m3 / h   Q  ≤ 59 m3 / h ===> NON_CONFORME
+    elsif (p_rec.debit > 15 AND p_rec.debit < 60)
+    then
+        select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'DEBIT_INSUFF_NC';
+        insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
+    --  Q > 1000 m3 / h ===> Trop elevée
+    elsif (p_rec.debit > 1000 )
+    then
+        select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'DEBIT_TROP_ELEVE';
+        insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
+    end if;
 
         -- Ajout anomalies PRESSION
     if (p_rec.pression is null OR p_rec.pression < 1) then
-    	select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'PRESSION_INSUFF';
-		insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
-	-- P 14 bar ===> Trop élevé
-	ELSIF (p_rec.pression > 14)
-		select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'PRESSION_TROP_ELEVEE';
-		insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
-	end if;
-	perform remocra.calcul_indispo(p_rec.id);
+        select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'PRESSION_INSUFF';
+        insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
+    -- P 14 bar ===> Trop élevé
+    ELSIF (p_rec.pression > 14) then
+        select id into p_anomalie_id from remocra.type_hydrant_anomalie where code = 'PRESSION_TROP_ELEVEE';
+        insert into remocra.hydrant_anomalies (hydrant,anomalies) values (p_rec.id, p_anomalie_id);
+    end if;
+    perform remocra.calcul_indispo(p_rec.id);
 END;
 $function$
 ;
