2d455c3196b3e94cc1f60119a1f85f826643e9d2
Emilie Genton <emg@atolcd.com>
2023-01-16 10:30:59 +0100

    Ajoute les patches de mise à jour des méthodes de numérotation pour 01, 14 + 39
    Change-Id: Ic1dfea56cb8755a4c2acf32ce73bb6962474929f

---
+++ server/sdis-remocra/home/postgres/remocra_db/patches/sdis/39/176_001.sql
@@ -0,0 +1,17 @@
+begin;
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = remocra, pdi, public, pg_catalog;
+
+
+update remocra.param_conf set valeur='39' where cle='HYDRANT_NUMEROTATION_METHODE';
+update remocra.param_conf set valeur='39' where cle='HYDRANT_NUMEROTATION_INTERNE_METHODE';
+
+
+commit;
