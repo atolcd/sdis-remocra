12a588b60189b0b6b1f8cef43a3bc879236d64bb
Thomas Broyer <tbr@atolcd.com>
2023-03-21 13:15:52 +0100

    Build api-remocra en dehors de Docker
    On bénéfie ainsi du cache des dépendances et de Nexus en proxy
    
    Change-Id: I167386718808ea1c30b298f59c0546df2dad9679

--- api-remocra/dist/create-image.sh
+++ api-remocra/dist/create-image.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)/..
 
 # Build artefacts and runnable image
-docker build . -t api-remocra --network=host -f dist/Dockerfile-image \
+docker build . -t api-remocra --network=host -f dist/Dockerfile \
   \
   || { echo 'Image build failed' ; exit 1; }
