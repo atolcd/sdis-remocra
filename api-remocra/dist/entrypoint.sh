d4cafba64321914986cdf12b6dbf4fc495f70f67
Thomas Broyer <tbr@atolcd.com>
2023-03-21 13:20:03 +0100

    Améliore l'entrypoint d'api-remocra pour ne plus chown le code
    Ça n'a pas d'intérêt, et au contraire une faille permettrait
    alors à l'application de modifier son propre code.
    
    On évite aussi de faire un 'cd', en utilisant plutôt des chemins
    absolus (en pratique, le WORKDIR de l'image nous place dans le
    même répertoire, donc tout chemin relatif sera équivalent)
    
    Change-Id: I08da15b2f63cafc36f4725f3e44d67a1bdcf39da

--- api-remocra/dist/entrypoint.sh
+++ api-remocra/dist/entrypoint.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-cd $(dirname $0)
-
 # TODO see -XX:MaxRAM=...
 
 # Used in log4j2.xml
@@ -14,8 +12,6 @@ mkdir -p "$GCLOG_DIR"
 export CONFIG_DIR=${CONFIG_DIR:-/app/config/}
 mkdir -p "$CONFIG_DIR"
 
-chown -R remocra:remocra /app
- 
 GC_OPTS="-XX:+DisableExplicitGC -XX:+ScavengeBeforeFullGC"
 GC_LOG_OPTS="-Xlog:gc*:file=${GCLOG_DIR}/api-remocra-gc.log::filecount=10,filesize=1024"
 JAVA_OPTS="$JAVA_OPTS -Dorg.jboss.logging.provider=slf4j"
@@ -31,6 +27,6 @@ JAVA_OPTS="$JAVA_OPTS -Xms${INITIAL_MEMORY:-500m} -Xmx${MAXIMUM_MEMORY:-1g} -XX:
 JAVA_OPTS="$JAVA_OPTS -Xverify:none -XX:+UseCompressedOops -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=80 -XX:+CMSScavengeBeforeRemark"
 
 exec $JAVA_HOME/bin/java  $JAVA_OPTS  $GC_LOG_OPTS $GC_OPTS \
-  -cp 'lib/*' fr.sdis83.remocra.WebApp \
+  -cp '/app/lib/*' fr.sdis83.remocra.WebApp \
   -l ${CONFIG_DIR}/log4j2.xml -c ${CONFIG_DIR}/api-remocra.conf \
   "$@"
