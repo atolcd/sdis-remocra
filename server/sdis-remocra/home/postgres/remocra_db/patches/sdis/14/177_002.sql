918b8b43e796d4bc5cc39eb97291ba96309e4a73
Clément GRENOT <cgr@atolcd.com>
2023-06-13 10:06:57 +0200

    SDIS 14 : Crée un table répertoriant les outils de mesure ...
    Pour la génération des courriers (Synthèse CT)
    Issue: #174457
    
    Change-Id: I0e17f01fdac214cf1e5b2e1cc520b642888a2227

---
+++ server/sdis-remocra/home/postgres/remocra_db/patches/sdis/14/177_002.sql
@@ -0,0 +1,6 @@
+-- Création d'un table répertoriant les outils de mesure
+CREATE TABLE remocra_referentiel.outil_mesure (
+    code varchar NOT NULL,
+    verification date NOT NULL,
+    nom_complet varchar NOT NULL
+);
