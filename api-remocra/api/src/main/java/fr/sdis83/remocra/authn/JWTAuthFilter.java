4994fffb9d899ef0b4c642d1d043718fa15f06cd
Hugo Bole <hbo@atolcd.com>
2023-07-11 10:41:56 +0200

    Prend en compte le nouveau rôle "ADMINISTRER" pour l'API...
    ... on va chercher les 3 flags dans la table, mais le rôle d'admin est directement géré au niveau de l'authFilter pour éviter un refactoring inutile sur toutes les méthodes
    
    Issue #182708
    
    Change-Id: I1a089fe2bd3043022a9798976c77b3c58e78c5f2

--- api-remocra/api/src/main/java/fr/sdis83/remocra/authn/JWTAuthFilter.java
+++ api-remocra/api/src/main/java/fr/sdis83/remocra/authn/JWTAuthFilter.java
@@ -7,6 +7,7 @@ import fr.sdis83.remocra.web.model.ImmutableExceptionResponse;
 import fr.sdis83.remocra.web.model.authn.OrganismeModel;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
+
 import java.lang.reflect.Method;
 import java.security.Principal;
 import java.util.List;
@@ -24,6 +25,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.jboss.resteasy.core.ResourceMethodInvoker;
 import org.slf4j.Logger;
@@ -35,15 +37,17 @@ public class JWTAuthFilter implements ContainerRequestFilter {
 
   private static final Logger logger = LoggerFactory.getLogger(JWTAuthFilter.class);
 
-  @Inject javax.inject.Provider<OrganismesRepository> organismeRepository;
+  @Inject
+  javax.inject.Provider<OrganismesRepository> organismeRepository;
 
-  @Inject javax.inject.Provider<JWTAuthUser> authUser;
+  @Inject
+  javax.inject.Provider<JWTAuthUser> authUser;
 
   @Override
   public void filter(ContainerRequestContext requestContext) {
     ResourceMethodInvoker methodInvoker =
-        (ResourceMethodInvoker)
-            requestContext.getProperty("org.jboss.resteasy.core.ResourceMethodInvoker");
+            (ResourceMethodInvoker)
+                    requestContext.getProperty("org.jboss.resteasy.core.ResourceMethodInvoker");
     Method method = methodInvoker.getMethod();
 
     if (method.isAnnotationPresent(PermitAll.class)) {
@@ -64,9 +68,9 @@ public class JWTAuthFilter implements ContainerRequestFilter {
     String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
     if (authorizationHeader == null || authorizationHeader.length() < 8) {
       requestContext.abortWith(
-          Response.status(401)
-              .entity("Authorization header is missing")
-              .build());
+              Response.status(401)
+                      .entity("Authorization header is missing")
+                      .build());
       return;
     }
     String jws = authorizationHeader.substring("Bearer".length()).trim();
@@ -83,20 +87,20 @@ public class JWTAuthFilter implements ContainerRequestFilter {
     }
     if (errorMsg != null) {
       requestContext.abortWith(
-          Response.status(401)
-              .entity(ImmutableExceptionResponse.of(Optional.ofNullable(errorMsg)))
-              .build());
+              Response.status(401)
+                      .entity(ImmutableExceptionResponse.of(Optional.ofNullable(errorMsg)))
+                      .build());
       return;
     }
 
     if (username == null || username.isEmpty()) {
       logger.info("Authentication subject is missing.");
       requestContext.abortWith(
-          Response.status(401)
-              .entity(
-                  ImmutableExceptionResponse.of(
-                      Optional.ofNullable("Authentication subject is missing.")))
-              .build());
+              Response.status(401)
+                      .entity(
+                              ImmutableExceptionResponse.of(
+                                      Optional.ofNullable("Authentication subject is missing.")))
+                      .build());
       return;
     }
 
@@ -104,79 +108,86 @@ public class JWTAuthFilter implements ContainerRequestFilter {
     if (o == null) {
       logger.info("Authentication failed for " + username + ".");
       requestContext.abortWith(
-          Response.status(401)
-              .type(MediaType.APPLICATION_JSON)
-              .entity(
-                  ImmutableExceptionResponse.of(
-                      Optional.ofNullable(username + " account not found.")))
-              .build());
+              Response.status(401)
+                      .type(MediaType.APPLICATION_JSON)
+                      .entity(
+                              ImmutableExceptionResponse.of(
+                                      Optional.ofNullable(username + " account not found.")))
+                      .build());
       return;
     }
 
     final UserInfo userInfo = ImmutableUserInfo.of(o.getId(), o.getEmail(), o.getRoles(), o.getType(), o.getTypeId());
     final UserPrincipal principal =
-        new UserPrincipal() {
-          @Override
-          public String getName() {
-            return userInfo.username();
-          }
+            new UserPrincipal() {
+              @Override
+              public String getName() {
+                return userInfo.username();
+              }
 
-          @Override
-          public Long getUserId() {
-            return userInfo.userId();
-          }
+              @Override
+              public Long getUserId() {
+                return userInfo.userId();
+              }
 
-          @Override
-          public UserInfo getUserInfo() {
-            return userInfo;
-          }
+              @Override
+              public UserInfo getUserInfo() {
+                return userInfo;
+              }
 
-          @Override
-          public List<UserRoles> roles() {
-            return userInfo.roles();
-          }
+              @Override
+              public List<UserRoles> roles() {
+                return userInfo.roles();
+              }
 
-          @Override
-          public String type() { return userInfo.type();}
+              @Override
+              public String type() {
+                return userInfo.type();
+              }
 
-          @Override
-          public Long typeId() {
-            return userInfo.typeId();
-          }
-        };
+              @Override
+              public Long typeId() {
+                return userInfo.typeId();
+              }
+            };
 
     final SecurityContext oldSC = requestContext.getSecurityContext();
     requestContext.setSecurityContext(
-        new SecurityContext() {
-          @Override
-          public Principal getUserPrincipal() {
-            return principal;
-          }
+            new SecurityContext() {
+              @Override
+              public Principal getUserPrincipal() {
+                return principal;
+              }
 
-          @Override
-          public boolean isUserInRole(String role) {
-            return userInfo.roles().toString().equals(role) || oldSC.isUserInRole(role);
-          }
+              @Override
+              public boolean isUserInRole(String role) {
+                return userInfo.roles().toString().equals(role) || oldSC.isUserInRole(role);
+              }
 
-          @Override
-          public boolean isSecure() {
-            return oldSC.isSecure();
-          }
+              @Override
+              public boolean isSecure() {
+                return oldSC.isSecure();
+              }
 
-          @Override
-          public String getAuthenticationScheme() {
-            return null;
-          }
-        });
+              @Override
+              public String getAuthenticationScheme() {
+                return null;
+              }
+            });
+
+    // Si l'organisme utilisateur est administrateur, on bypass tout le reste
+    if (userInfo.roles().contains(UserRoles.ADMINISTRER)) {
+      return;
+    }
 
     RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
     boolean hasRight = false;
-    for(UserRoles r : userInfo.roles()) {
-      if(ArrayUtils.contains(rolesAnnotation.value(), r.toString())) {
+    for (UserRoles r : userInfo.roles()) {
+      if (ArrayUtils.contains(rolesAnnotation.value(), r.toString())) {
         hasRight = true;
       }
     }
-    if(!hasRight) {
+    if (!hasRight) {
       logger.info("Authorizations failed for {}.", username);
       requestContext.abortWith(Response.status(403).type(MediaType.APPLICATION_JSON).build());
     }
--- api-remocra/api/src/main/java/fr/sdis83/remocra/repository/OrganismesRepository.java
+++ api-remocra/api/src/main/java/fr/sdis83/remocra/repository/OrganismesRepository.java
@@ -21,75 +21,73 @@ import static fr.sdis83.remocra.db.model.remocra.Tables.TYPE_ORGANISME;
 
 public class OrganismesRepository {
 
-  private final DSLContext context;
+    private final DSLContext context;
 
-  @Inject
-  public OrganismesRepository(DSLContext context) {
-    this.context = context;
-  }
-
-  public String getAll(String codeNature, Integer start, Integer limit) throws JsonProcessingException {
-
-      Condition condition = this.getConditions(codeNature);
-
-      List<OrganismesModel> list = context
-        .select(ORGANISME.CODE.as("code"), ORGANISME.NOM.as("nom"), TYPE_ORGANISME.CODE.as("codeNature"))
-        .from(ORGANISME)
-        .join(TYPE_ORGANISME).on(ORGANISME.TYPE_ORGANISME.eq(TYPE_ORGANISME.ID))
-        .where(condition)
-        .limit((limit == null || limit < 0) ? this.count() : limit)
-        .offset((start == null || start < 0) ? 0 : start)
-        .fetchInto(OrganismesModel.class);
-
-      return new ObjectMapper().writeValueAsString(list);
+    @Inject
+    public OrganismesRepository(DSLContext context) {
+        this.context = context;
     }
 
-  private Integer count() {
-    return context.fetchCount(ORGANISME);
-  }
+    public String getAll(String codeNature, Integer start, Integer limit) throws JsonProcessingException {
 
-  private Condition getConditions(String codeNature){
+        Condition condition = this.getConditions(codeNature);
 
-    Condition condition = DSL.trueCondition();
-    if (codeNature != null) {
-      condition = condition.and(TYPE_ORGANISME.CODE.eq(codeNature.toUpperCase()));
-    }
-    return condition;
-  }
+        List<OrganismesModel> list = context
+                .select(ORGANISME.CODE.as("code"), ORGANISME.NOM.as("nom"), TYPE_ORGANISME.CODE.as("codeNature"))
+                .from(ORGANISME)
+                .join(TYPE_ORGANISME).on(ORGANISME.TYPE_ORGANISME.eq(TYPE_ORGANISME.ID))
+                .where(condition)
+                .limit((limit == null || limit < 0) ? this.count() : limit)
+                .offset((start == null || start < 0) ? 0 : start)
+                .fetchInto(OrganismesModel.class);
 
-  public OrganismeModel readByEmail(String email) {
-    OrganismeModel o = context
-      .select(ORGANISME.ID, ORGANISME.EMAIL_CONTACT.as("email"), ORGANISME.PASSWORD, ORGANISME.SALT,
-        TYPE_ORGANISME.ID.as("typeId"), TYPE_ORGANISME.CODE.as("type"))
-      .from(ORGANISME)
-      .join(TYPE_ORGANISME).on(ORGANISME.TYPE_ORGANISME.eq(TYPE_ORGANISME.ID))
-      .where(ORGANISME.EMAIL_CONTACT.equalIgnoreCase(email.toUpperCase()))
-      .fetchOneInto(OrganismeModel.class);
-
-    if(o != null) {
-      List<UserRoles> roles = new ArrayList<UserRoles>();
-
-      TransfertsAutomatises transfertsAutomatises =
-              context
-                      .selectFrom(TRANSFERTS_AUTOMATISES)
-                      .where(TRANSFERTS_AUTOMATISES.TYPE_ORGANISME.eq(o.getTypeId()))
-                      .fetchOneInto(TransfertsAutomatises.class);
-
-      if(transfertsAutomatises.getRecuperer()) {
-        roles.add(UserRoles.RECEVOIR);
-      }
-
-      if(transfertsAutomatises.getTransmettre()) {
-        roles.add(UserRoles.TRANSMETTRE);
-      }
-
-      if(transfertsAutomatises.getAdministrer()) {
-        roles.add(UserRoles.ADMINISTRER);
-      }
-
-      o.setRoles(roles);
+        return new ObjectMapper().writeValueAsString(list);
     }
 
-    return o;
-  }
+    private Integer count() {
+        return context.fetchCount(ORGANISME);
+    }
+
+    private Condition getConditions(String codeNature) {
+
+        Condition condition = DSL.trueCondition();
+        if (codeNature != null) {
+            condition = condition.and(TYPE_ORGANISME.CODE.eq(codeNature.toUpperCase()));
+        }
+        return condition;
+    }
+
+    public OrganismeModel readByEmail(String email) {
+        OrganismeModel o = context
+                .select(ORGANISME.ID, ORGANISME.EMAIL_CONTACT.as("email"), ORGANISME.PASSWORD, ORGANISME.SALT,
+                        TYPE_ORGANISME.ID.as("typeId"), TYPE_ORGANISME.CODE.as("type"))
+                .from(ORGANISME)
+                .join(TYPE_ORGANISME).on(ORGANISME.TYPE_ORGANISME.eq(TYPE_ORGANISME.ID))
+                .where(ORGANISME.EMAIL_CONTACT.equalIgnoreCase(email.toUpperCase()))
+                .fetchOneInto(OrganismeModel.class);
+
+        if (o != null) {
+            List<UserRoles> roles = new ArrayList<UserRoles>();
+
+            TransfertsAutomatises transfertsAutomatises =
+                    context
+                            .selectFrom(TRANSFERTS_AUTOMATISES)
+                            .where(TRANSFERTS_AUTOMATISES.TYPE_ORGANISME.eq(o.getTypeId()))
+                            .fetchOneInto(TransfertsAutomatises.class);
+
+            if (transfertsAutomatises.getRecuperer()) {
+                roles.add(UserRoles.RECEVOIR);
+            }
+
+            if (transfertsAutomatises.getTransmettre()) {
+                roles.add(UserRoles.TRANSMETTRE);
+            }
+
+            if (transfertsAutomatises.getAdministrer()) {
+                roles.add(UserRoles.ADMINISTRER);
+            }
+            o.setRoles(roles);
+        }
+        return o;
+    }
 }
