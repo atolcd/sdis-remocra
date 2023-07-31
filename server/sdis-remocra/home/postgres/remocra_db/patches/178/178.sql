da28b4e39c21bc7f75995401e6bcf5ba223445d0
Emilie Genton <emg@atolcd.com>
2023-07-31 08:27:44 +0200

    Ajoute un rôle admin dans la base pour la gestion de l'API
    Issue #182708
    
    Change-Id: I741119fc37c731ecbd7b46608308d7f216e01f9d

---
+++ server/sdis-remocra/home/postgres/remocra_db/patches/178/178.sql
@@ -0,0 +1,34 @@
+begin;
+set statement_timeout = 0;
+set client_encoding = 'UTF8';
+set standard_conforming_strings = off;
+set check_function_bodies = false;
+set client_min_messages = warning;
+set escape_string_warning = off;
+set search_path = remocra, pdi, public, pg_catalog;
+--------------------------------------------------
+-- Versionnement du patch et vérification
+--
+create or replace function versionnement_dffd4df4df() returns void language plpgsql AS $body$
+declare
+    numero_patch int;
+    description_patch varchar;
+begin
+    -- Métadonnées du patch
+    numero_patch := 178;
+    description_patch := 'Ajout d\'un rôle dans la table de transferts_automatises pour une administration générale;
+    -- Vérification
+    if (select numero_patch-1 != (select max(numero) from remocra.suivi_patches)) then
+        raise exception 'Le numéro de patch requis n''est pas le bon. Dernier appliqué : %, en cours : %', (select max(numero) from remocra.suivi_patches), numero_patch; end if;
+    -- Suivi
+    insert into remocra.suivi_patches(numero, description) values(numero_patch, description_patch);
+end $body$;
+select versionnement_dffd4df4df();
+drop function versionnement_dffd4df4df();
+--------------------------------------------------
+-- Contenu réel du patch début
+alter table remocra.transferts_automatises
+add administrer boolean default false;
+-- Contenu réel du patch fin
+--------------------------------------------------
+COMMIT;
