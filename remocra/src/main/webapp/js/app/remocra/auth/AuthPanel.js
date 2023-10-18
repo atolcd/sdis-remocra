78af099e8e8118f3067f28e7c709511ca540a5c6
Renaud KIEFFER <rki@atolcd.com>
2023-10-18 12:19:55 +0200

    Améliore la sécurité de la page de login
    Avant :
    
      -le bouton "Choix du mot de passe" ne renvoyait pas le même
      message si le login existe ou non.
      - le bouton "Choix du mot de passe" ouvrait une popup avec le nom
    d'utilisateur mais pas escape correctement
    
    Issue: #192904
    Change-Id: I500e541a7bf7527caea518b40c95a1d60fea208a

--- remocra/src/main/webapp/js/app/remocra/auth/AuthPanel.js
+++ remocra/src/main/webapp/js/app/remocra/auth/AuthPanel.js
@@ -7,14 +7,14 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
     extend: 'Ext.Panel',
     requires: [],
     alias: 'widget.remocra.authpanel',
-    
+
     id: 'authPanel',
     cls: 'remocra-authpanel',
-    
+
     border: false, defaults: {border: false, style: 'margin:5px;'},
     inAWindow: true,
-    
-    initComponent: function() {
+
+    initComponent: function () {
         // -- EVENEMENTS
         this.addEvents(
             /**
@@ -30,11 +30,11 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
              */
             'cancel'
         );
-        var msg = {hidden:true};
-        if (this.msg!==false) {
+        var msg = {hidden: true};
+        if (this.msg !== false) {
             msg = this.msg || 'Veuillez saisir votre identifiant et votre mot de passe pour vous identifier.';
         }
-        
+
         // -- PRESENTATION
         var presPanel = {cls: 'auth-pres', html: msg};
         var errorPanel = {itemId: 'errorPanel', cls: 'auth-error', hidden: true, html: ''};
@@ -47,35 +47,37 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
             border: false,
             defaults: {labelSeparator: '', allowBlank: false},
             items: [{
-                xtype: 'label', text: 'Nom d\'utilisateur', hidden:this.inAWindow!==false
+                xtype: 'label', text: 'Nom d\'utilisateur', hidden: this.inAWindow !== false
             }, {
                 itemId: 'loginF', width: 300,
-                fieldLabel: this.inAWindow!==false?'Nom d\'utilisateur':null,
+                fieldLabel: this.inAWindow !== false ? 'Nom d\'utilisateur' : null,
                 blankText: 'Veuillez saisir votre identifiant',
                 value: userLogin
             }, {
-                xtype: 'label', text: 'Mot de passe', hidden:this.inAWindow!==false
+                xtype: 'label', text: 'Mot de passe', hidden: this.inAWindow !== false
             }, {
                 itemId: 'passwordF', width: 300,
-                fieldLabel: this.inAWindow!==false?'Mot de passe':null,
+                fieldLabel: this.inAWindow !== false ? 'Mot de passe' : null,
                 inputType: 'password',
                 blankText: 'Veuillez saisir votre mot de passe',
-                enableKeyEvents:true,
-                enterIsSpecial : true,
-                scope : this,
-                listeners:{
-                    'specialkey':function(me, e){
-                        if (e.getKey() == e.ENTER) {
+                enableKeyEvents: true,
+                enterIsSpecial: true,
+                scope: this,
+                listeners: {
+                    'specialkey': function (me, e) {
+                        if (e.getKey() === e.ENTER) {
                             this.scope.beforeOk.call(this.scope);
                         }
                     }
                 }
             }],
-            
+
             listeners: {
-                render: function() {
-                    var fieldFocusId = userLogin!=''?'passwordF':'loginF';
-                    Ext.Function.defer(function(){this.getComponent('form').getComponent(fieldFocusId).focus();}, 100, this);
+                render: function () {
+                    var fieldFocusId = userLogin !== '' ? 'passwordF' : 'loginF';
+                    Ext.Function.defer(function () {
+                        this.getComponent('form').getComponent(fieldFocusId).focus();
+                    }, 100, this);
                 }, scope: this
             },
             keys: [{
@@ -83,56 +85,64 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
                 fn: Ext.bind(this.beforeOk, this)
             }]
         });
-        
+
         // -- BOUTONS
         var actionButtons = [];
         actionButtons.push(new Ext.Button({
-            id : 'okBtn'+this.id,
-            text : 'S\'identifier',
-            scale : this.inAWindow!==false ? 'small' : 'large',
-            minWidth : this.inAWindow!==false ? 70 : 120,
+            id: 'okBtn' + this.id,
+            text: 'S\'identifier',
+            scale: this.inAWindow !== false ? 'small' : 'large',
+            minWidth: this.inAWindow !== false ? 70 : 120,
             listeners: {
                 click: Ext.bind(this.beforeOk, this)
             }
         }));
-        if (this.inAWindow!==false) {
+        if (this.inAWindow !== false) {
             actionButtons.push(new Ext.Button({
-                id : 'cancelBtn'+this.id,
-                text : 'Fermer',
-                minWidth  : 70,
+                id: 'cancelBtn' + this.id,
+                text: 'Fermer',
+                minWidth: 70,
                 listeners: {
-                    click: function(){ this.fireEvent('cancel', this); },
+                    click: function () {
+                        this.fireEvent('cancel', this);
+                    },
                     scope: this
                 }
             }));
         }
         actionButtons.push({
             xtype: 'box',
-            autoEl: { tag: 'a', style: 'margin-left:20px;', href:'', onclick: 'Ext.getCmp(\''+this.id+'\').passwordForgotten();return false;', html: 'Choix du mot de passe' }
+            autoEl: {
+                tag: 'a',
+                style: 'margin-left:20px;',
+                href: '',
+                onclick: 'Ext.getCmp(\'' + this.id + '\').passwordForgotten();return false;',
+                html: 'Choix du mot de passe'
+            }
         });
-        
+
         Ext.apply(this, {
             items: [presPanel, formPanel, errorPanel],
             buttons: actionButtons,
-            buttonAlign: this.inAWindow?'center':'left'
+            buttonAlign: this.inAWindow ? 'center' : 'left'
         });
 
         this.callParent(arguments);
     },
-    
+
     // ---
     // ACTIONS
     // ---
-    
-    beforeOk: function() {
+
+    beforeOk: function () {
         var expires = new Date();
-        expires.setDate(expires.getDate()+7); // Expiration à 7 jours
+        expires.setDate(expires.getDate() + 7); // Expiration à 7 jours
         Ext.util.Cookies.set("loginUser", this.getComponent('form').getComponent('loginF').getValue(), expires);
         this.fireEvent('ok', this);
     },
-    
+
     // Déplacées dans la fenêtre
-    showHideErrorMsg: function(msg) {
+    showHideErrorMsg: function (msg) {
         var errorPanel = this.getComponent("errorPanel");
         if (msg) {
             errorPanel.update(msg);
@@ -141,11 +151,24 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
             errorPanel.setVisible(false);
         }
     },
-    popUpDemandeResetMdp: function(login) {
-       Ext.Msg.confirm('Authentification', 'Confirmez vous la demande de réinitialisation de mot de passe pour "'+login+'" ?'
-        + '<br/><p style="margin-top:10px;margin-left:20px;font-style:italic;color:#a9a9a9;">'
-        + 'En choississant "Oui", vous recevrez un courriel qui vous permettra de modifier votre mot de passe.</p>', function(btn) {
-            if (btn == "yes"){
+    escapeHtml: function (text) {
+        var map = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#039;'
+        };
+
+        return text.replace(/[&<>"']/g, function (m) {
+            return map[m];
+        });
+    },
+    popUpDemandeResetMdp: function (login) {
+        Ext.Msg.confirm('Authentification', 'Confirmez vous la demande de réinitialisation de mot de passe pour "' + this.escapeHtml(login) + '" ?'
+            + '<br/><p style="margin-top:10px;margin-left:20px;font-style:italic;color:#a9a9a9;">'
+            + 'En choississant "Oui", vous recevrez un courriel qui vous permettra de modifier votre mot de passe.</p>', function (btn) {
+            if (btn === "yes") {
                 Ext.Ajax.request({
                     url: Sdis.Remocra.util.Util.withBaseUrl('../utilisateurs/lostpassword'),
                     method: 'POST',
@@ -153,30 +176,20 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
                         identifiant: login
                     },
                     scope: this,
-                    callback: function(options, success, response) {
-                        var jsResp = Ext.decode(response.responseText);
-                        if (!success || !jsResp.success) {
-                            Ext.MessageBox.show({
-                                title: 'Authentification',
-                                msg: 'Impossible de réaliser la demande avec cet identifiant.',
-                                buttons: Ext.Msg.OK,
-                                icon: Ext.MessageBox.ERROR
-                            });
-                        } else {
-                            Ext.MessageBox.show({
-                                title: 'Authentification',
-                                msg: 'La demande de réinitialisation de mot de passe a été transmise.'
-                                    + ' Vous devriez recevoir un courriel qui vous permettra de modifier votre mot de passe.',
-                                buttons: Ext.Msg.OK,
-                                icon: Ext.MessageBox.INFO
-                            });
-                        }
+                    callback: function (options, success, response) {
+                        Ext.MessageBox.show({
+                            title: 'Authentification',
+                            msg: 'La demande de réinitialisation de mot de passe a été transmise.'
+                                + ' Vous devriez recevoir un courriel qui vous permettra de modifier votre mot de passe.',
+                            buttons: Ext.Msg.OK,
+                            icon: Ext.MessageBox.INFO
+                        });
                     }
                 });
             }
         }, this);
     },
-    passwordForgotten: function() {
+    passwordForgotten: function () {
         var loginF = this.getComponent('form').getComponent('loginF');
         if (loginF.isValid() !== true) {
             this.showHideErrorMsg("Merci de renseigner votre nom d'utilisateur");
@@ -195,7 +208,7 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
                 identifiant: login
             },
             scope: this,
-            callback: function(options, success, response) {
+            callback: function (options, success, response) {
                 var jsResp = Ext.decode(response.responseText);
 
                 // Si le mot de passe n'est pas LDAP
@@ -203,7 +216,7 @@ Ext.define('Sdis.Remocra.auth.AuthPanel', {
                     this.popUpDemandeResetMdp(login);
                 }
                 // SI LDAP
-                else if (jsResp == true)  {
+                else if (jsResp === true) {
                     Ext.MessageBox.show({
                         title: 'Utilisateur non autorisé',
                         msg: 'Veuillez contacter votre SDIS pour changer votre mot de passe.',
