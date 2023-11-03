7f0e338d7a317bae177855c0e95ff40a15242c2c
Emilie Genton <emg@atolcd.com>
2023-11-03 08:33:42 +0100

    Augmente la version de l'appli mobile
    Change-Id: I82dc9b95b9cdb01ad81b73f315c5b9e193ff7f5c

--- api-remocra/api/src/main/java/fr/sdis83/remocra/authn/CompatibleVersions.java
+++ api-remocra/api/src/main/java/fr/sdis83/remocra/authn/CompatibleVersions.java
@@ -12,7 +12,7 @@ public class CompatibleVersions {
 
   // Versions autorisées pour l'appli mobile
   private final List<String> mobileCompatibleVersions =
-      Collections.singletonList(MobileVersion.M2_0_beta2.version);
+      Collections.singletonList(MobileVersion.M2_0_beta3.version);
 
   public CompatibleVersions() {}
 
@@ -32,7 +32,7 @@ public class CompatibleVersions {
   }
 
   enum MobileVersion {
-    M2_0_beta2("2.0-beta2");
+    M2_0_beta3("2.0-beta3");
 
     public final String version;
 
