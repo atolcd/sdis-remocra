1a0a9f89195c028be2a758e6c634a5e55042eecf
Clément GRENOT <cgr@atolcd.com>
2024-01-19 11:55:12 +0100

    [SDIS 61] Corrige le patch 195_001.sql
    Issue: #203701
    Change-Id: I84525cee7160e8e4b75358914996cd69161889da

--- server/sdis-remocra/home/postgres/remocra_db/patches/sdis/61/195_001.sql
+++ server/sdis-remocra/home/postgres/remocra_db/patches/sdis/61/195_001.sql
@@ -1,15 +1,13 @@
-DROP VIEW remocra_sgo.remocrartemis
-
-
+DROP VIEW remocra_sgo.remocrartemis;
 
 CREATE OR REPLACE VIEW remocra_sgo.remocrartemis
 AS WITH anomalie AS (
          SELECT h_1.id,
             array_to_string(array_agg(tha.code), ' | '::text) AS liste_anomalie_code,
             array_to_string(array_agg(tha.nom), ' | '::text) AS liste_anomalie_nom
-           FROM hydrant h_1
-             LEFT JOIN hydrant_anomalies ha ON h_1.id = ha.hydrant
-             LEFT JOIN type_hydrant_anomalie tha ON ha.anomalies = tha.id
+           FROM remocra.hydrant h_1
+             LEFT JOIN remocra.hydrant_anomalies ha ON h_1.id = ha.hydrant
+             LEFT JOIN remocra.type_hydrant_anomalie tha ON ha.anomalies = tha.id
           GROUP BY h_1.id
         )
  SELECT h.numero AS num,
@@ -34,17 +32,17 @@ AS WITH anomalie AS (
     st_y(h.geometrie) AS coord_y,
     o_tournee.code AS code_organisme_tournee,
     o_tournee.nom AS nom_organisme_tournee
-   FROM hydrant h
-     JOIN type_hydrant_nature thn ON h.nature = thn.id
-     JOIN type_hydrant_nature_deci thnd ON h.nature_deci = thnd.id
-     LEFT JOIN type_hydrant_domaine dom ON h.domaine = dom.id
-     LEFT JOIN hydrant_pibi pibi ON h.id = pibi.id
-     LEFT JOIN hydrant_pena pena ON h.id = pena.id
-     LEFT JOIN type_hydrant_diametre thd ON pibi.diametre = thd.id
+   FROM remocra.hydrant h
+     JOIN remocra.type_hydrant_nature thn ON h.nature = thn.id
+     JOIN remocra.type_hydrant_nature_deci thnd ON h.nature_deci = thnd.id
+     LEFT JOIN remocra.type_hydrant_domaine dom ON h.domaine = dom.id
+     LEFT JOIN remocra.hydrant_pibi pibi ON h.id = pibi.id
+     LEFT JOIN remocra.hydrant_pena pena ON h.id = pena.id
+     LEFT JOIN remocra.type_hydrant_diametre thd ON pibi.diametre = thd.id
      LEFT JOIN anomalie ON anomalie.id = h.id
-     JOIN commune c ON h.commune = c.id
-     JOIN organisme o_transi ON c.nom::text = o_transi.nom::text
-     JOIN organisme o ON o.id = o_transi.organisme_parent
-     LEFT JOIN hydrant_tournees ht ON h.id = ht.hydrant
-     LEFT JOIN tournee t ON ht.tournees = t.id
-     LEFT JOIN organisme o_tournee ON t.affectation = o_tournee.id;
+     JOIN remocra.commune c ON h.commune = c.id
+     JOIN remocra.organisme o_transi ON c.nom::text = o_transi.nom::text
+     JOIN remocra.organisme o ON o.id = o_transi.organisme_parent
+     LEFT JOIN remocra.hydrant_tournees ht ON h.id = ht.hydrant
+     LEFT JOIN remocra.tournee t ON ht.tournees = t.id
+     LEFT JOIN remocra.organisme o_tournee ON t.affectation = o_tournee.id;
