74856eb6c9bcb5d730c844fc8211547fa8f6a213
Emilie Genton <emg@atolcd.com>
2024-05-02 08:45:07 +0200

    Change la compatible version de l'application mobile en 2.2
    Change-Id: I99d82c3940e2b4b0398639cba1b1e6765ed63202

--- api-remocra/api/src/main/java/fr/sdis83/remocra/authn/CompatibleVersions.java
+++ api-remocra/api/src/main/java/fr/sdis83/remocra/authn/CompatibleVersions.java
@@ -12,7 +12,7 @@ public class CompatibleVersions {
 
   // Versions autorisées pour l'appli mobile
   private final List<String> mobileCompatibleVersions =
-      Collections.singletonList(MobileVersion.M2_1.version);
+      Collections.singletonList(MobileVersion.M2_2.version);
 
   public CompatibleVersions() {}
 
@@ -33,7 +33,8 @@ public class CompatibleVersions {
 
   enum MobileVersion {
     M2_0("2.0"),
-    M2_1("2.1");
+    M2_1("2.1"),
+    M2_2("2.2");
 
     public final String version;
 
