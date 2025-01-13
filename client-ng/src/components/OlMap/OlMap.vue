ccae2831dc53ddd4caee95ef027fc0090917e4ab
Emilie Genton <emg@atolcd.com>
2025-01-13 14:30:21 +0100

    Corrige l'affichage de la couverture hydraulique et du module de crise
    Issue: #238511
    Change-Id: I8278464b89f2ed53e1fe3e9414ef0cfc669f70fc

--- client-ng/src/components/OlMap/OlMap.vue
+++ client-ng/src/components/OlMap/OlMap.vue
@@ -126,7 +126,7 @@ export default {
 
     this.proj = this.map.getView().getProjection()
     this.epsg = 'EPSG:' + this.srid;
-    switch(srid) {
+    switch(this.srid) {
         case "2154":
             proj4.defs(this.epsg, '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')
             break;
